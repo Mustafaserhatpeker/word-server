da2d8340167e905894498d0a3b8c58b83139f762
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-04-20 23:45:20 +0300

    Add auth routes for user registration and update index routing

--- fizyo-back/src/routes/userRoutes.js
+++ fizyo-back/src/routes/userRoutes.js
@@ -1,10 +1,10 @@
 // src/routes/userRoutes.js
 import express from 'express';
-import { getAllUsersController, registerController } from '../controllers/userController.js';
+import { getAllUsersController} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/get-all-users', getAllUsersController);
-router.post('/register', registerController);
+
 
 export default router;
