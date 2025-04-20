2fe85cc61b7f4dc3d0ee33f8342847d85b75ef63
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-04-21 02:22:34 +0300

    fix: update server listen address to '0.0.0.0' for external access

--- fizyo-back/server.js
+++ fizyo-back/server.js
@@ -5,7 +5,7 @@ import { connectDB } from './src/config/database.js';
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT,'0.0.0.0', () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
   });
 });
