6e8a7262570513df83f5102a8939f86098033878
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-04-24 14:52:42 +0300

    Add doctorController with registration and login functionality for doctors

---
+++ fizyo-back/src/controllers/doctorController.js
@@ -0,0 +1,20 @@
+import { loginDoctor,registerDoctor } from "../services/doctorService";
+
+import { sendResponse } from "../utils/sendResponse.js";
+import { catchAsync } from "../utils/catchAsync.js";
+
+export const registerDoctorController = catchAsync(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await registerDoctor(username, password);
+
+  sendResponse(res, 201, user, "Doktor başarıyla kaydedildi.");
+}
+);
+export const loginDoctorController = catchAsync(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await loginDoctor(username, password);
+
+  sendResponse(res, 200, user, "Doktor başarıyla giriş yaptı.");
+});
