8a0896541d1f10461dcf4ed2403428bc9269d669
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-04-24 14:43:10 +0300

    Add Doctor model with password hashing and comparison methods

---
+++ fizyo-back/src/models/Doctor.js
@@ -0,0 +1,39 @@
+// src/models/doctor.js
+
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const doctorSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false, // şifre veritabanında varsayılan olarak döndürülmez
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+// Şifreyi hash'leme (pre-save middleware)
+doctorSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // Şifre değiştirilmediyse, işleme devam etme
+
+  const salt = await bcrypt.genSalt(10); // 10, bcrypt'teki salt rounds sayısıdır
+  this.password = await bcrypt.hash(this.password, salt); // Şifreyi hash'le
+  next();
+});
+
+// Şifreyi kontrol etme metodu
+doctorSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password); // Şifreyi karşılaştır
+};
+
+const Doctor = mongoose.model('doctor', doctorSchema);
+
+export default Doctor;
