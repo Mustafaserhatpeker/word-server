88a902d8e88c3f840496deeade35c03fb5393166
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-04-26 18:11:03 +0300

    fix: remove unnecessary comment from findMatchController

--- word-back/src/controllers/matchmakingController.js
+++ word-back/src/controllers/matchmakingController.js
@@ -6,7 +6,7 @@ import { sendResponse } from '../utils/sendResponse.js';
 
 export const findMatchController = catchAsync(async (req, res, next) => {
   const { duration } = req.body;
-  const userId = req.user.id; // JWT'den gelen kullanıcı
+  const userId = req.user.id; 
 
   const game = await matchmakingService.findOrCreateMatch(userId, duration);
 
