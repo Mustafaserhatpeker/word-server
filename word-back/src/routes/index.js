c3a47c366db34e7724dc06d86734fd3115a8b5e6
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-05-03 22:16:01 +0300

    refactor: remove game and matchmaking controllers, models, services, and routes

--- word-back/src/routes/index.js
+++ word-back/src/routes/index.js
@@ -1,15 +1,13 @@
 import express from 'express';
-import userRoutes from './userRoutes.js'; 
+import userRoutes from './userRoutes.js';
 import authRoutes from './authRoutes.js';
-import gameRoutes from './gameRoutes.js';
-import matchmakingRoutes from './matchmakingRoutes.js';
+
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 router.use('/auth', authRoutes);
-router.use('/game', gameRoutes);
-router.use('/matchmaking', matchmakingRoutes);
+
 
 
 export default router;
